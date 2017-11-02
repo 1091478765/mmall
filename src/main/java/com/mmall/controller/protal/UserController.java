05dc1552826828966906acb216359dd375b282ff
1091478765 <1091478765@qq.com>
2017-11-02 08:15:09 +0800

    common class commit

---
+++ src/main/java/com/mmall/controller/protal/UserController.java
@@ -0,0 +1,12 @@
+package com.mmall.controller.protal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 刘璐 on 2017/11/2.
+ */
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+}
